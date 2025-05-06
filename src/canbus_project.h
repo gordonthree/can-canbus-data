1aaf73c3dfcbce7492f551af7ddde27a1f1ebbf0
Gordon McLellan <gordonthree@gmail.com>
2025-05-06 01:02:51 -0400

    one #include to rule them all

---
+++ src/canbus_project.h
@@ -0,0 +1,10 @@
+#ifndef CANBUS_PROJECT_H
+#define CANBUS_PROJECT_H
+
+#include "canbus_defs.h"
+#include "canbus_flags.h"
+#include "canbus_msg.h"
+#include "canbus_struct.h"
+
+
+#endif // end CANBUS_PROJECT_H
