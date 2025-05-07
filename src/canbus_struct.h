682e6048434b30504b41010625ec00ac29309db6
Gordon McLellan <gordonthree@gmail.com>
2025-05-07 17:05:05 -0400

    comment cleanup, add counter for each sub module

--- src/canbus_struct.h
+++ src/canbus_struct.h
@@ -75,31 +75,32 @@ struct outputSwitch {
   **/
   struct canNodeInfo {                                 
     
-    uint16_t  nodeType             = 0;                 /**  11-bit message id that defines the node type, used for introduction, set to 0 if node not present */
-    uint8_t   nodeID[NODE_ID_SIZE] = {0,0,0,0};         /**  unique 32-bit node id number */
-    uint8_t   featureMask[2]       = {0,0};             /**  two bytes of data to send with node introduction (optional) */
-    uint8_t   subModCnt            = 0;                 /**  sub module count */
-    time_t    lastSeen             = 0;                 /**  last time a message was received from node  */
-    time_t    firstSeen            = 0;                 /**  first time message received from node */
+    uint16_t  nodeType             = 0;                 /**  An 11-bit message id that defines the node type, used for introduction, set to 0 if node not present. */
+    uint8_t   nodeID[NODE_ID_SIZE] = {0,0,0,0};         /**  Unique 32-bit node id number. */
+    uint8_t   featureMask[2]       = {0,0};             /**  Two bytes of data to send with node introduction (optional.) */
+    uint8_t   subModCnt            = 0;                 /**  Sub module count for this node. */
+    time_t    lastSeen             = 0;                 /**  Last time a message was received from this node.  */
+    time_t    firstSeen            = 0;                 /**  First time a message was received from this node. */
     
-    struct    {                                         /** storage for sub modules */
-                uint16_t modType         = 0;           /** 11-bit message id that defines module type, used for introduction  */
-                uint8_t  featuresMask[2] = {0,0};       /** feature mask to send with introduction  */
-                bool     sendFeatureMask = false;       /** send feature mask during introduction */
-                uint16_t txMsgID         = 0;           /** module sends data using this message id  */
-                bool     privMsg         = false;       /** flag indicating txMsgID is a private channel  */
-                uint8_t  u8Value         = 0;           /** byte data field use for switch state or sensor data  */
-                int32_t  i32Value        = 0;           /** signed int data field  */
-                float    fltValue        = 0.0;         /** floating point data field  */
-                uint8_t  dataSize        = 4;           /** sensor data size in bytes, more than 4 requires a private message  */
-                uint8_t  outMode         = 0;           /** switch mode 0 toggle, 1 momentary, 2 blinking, 3 strobe, 4 pwm, 5 disabled USE DEFINES  */
-                uint16_t pwmDuty         = 0;           /** pwm duty cycle  */
-                uint16_t pwmFreq         = 1000;        /** pwm frequency  */
-                uint16_t blinkDelay      = 5000;        /** blink delay in ms   */
-                uint16_t momPressDur     = 500;         /** momentary press duration in ms   */
-                uint8_t  strobePat       = 0;           /** strobe pattern USE DEFINES  */
-                uint8_t  stateMemory     = 1;           /** state memory USE DEFINES  */
-                time_t   timestamp       = 0;           /** last seen timestamp  */
+    struct    {                                         /** Storage for sub modules. */
+                uint16_t modType         = 0;           /** 11-bit message id that defines module type, used for introduction.  */
+                uint8_t  modCount        = 0;           /** Number of modules associated with the current modType. This information might be included in the feature mask. */
+                uint8_t  featuresMask[2] = {0,0};       /** Feature mask to send with introduction.  */
+                bool     sendFeatureMask = false;       /** Send feature mask during introduction. */
+                uint16_t txMsgID         = 0;           /** Module sends data using this message id.  */
+                bool     privMsg         = false;       /** Flag indicating txMsgID is a private channel.  */
+                uint8_t  u8Value         = 0;           /** Byte data field use for switch state or sensor data.  */
+                int32_t  i32Value        = 0;           /** Signed int data field.  */
+                float    fltValue        = 0.0;         /** Floating point data field.  */
+                uint8_t  dataSize        = 4;           /** Densor data size in bytes, more than 4 requires a private message.  */
+                uint8_t  outMode         = 0;           /** Current output mode - see defines. */
+                uint16_t pwmDuty         = 0;           /** Current PWM duty cycle.  */
+                uint16_t pwmFreq         = 1000;        /** Current PWM frequency.  */
+                uint16_t blinkDelay      = 5000;        /** Blink delay in milliseconds.   */
+                uint16_t momPressDur     = 500;         /** Momentary press duration in milliseconds.  */
+                uint8_t  strobePat       = 0;           /** Strobe pattern - see defines.  */
+                uint8_t  stateMemory     = 1;           /** Output state memory - see defines.  */
+                time_t   timestamp       = 0;           /** Last seen timestamp for this module.  */
               } subModules[8];  
   };    // end canNodeInfo 
 
