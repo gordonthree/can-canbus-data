47140a51c4667684a0bde7266cfdc48d15846791
Gordon McLellan <gordonthree@gmail.com>
2026-02-13 12:05:47 -0500

    declare the flags, but define them in main.cpp

--- include/canbus_flags.h
+++ include/canbus_flags.h
@@ -1,13 +1,16 @@
 #ifndef CAN_FLAGS_H
 #define CAN_FLAGS_H
-//
 
-uint8_t FLAG_SEND_INTRODUCTION;    // stop introduction messages
-uint8_t FLAG_BEGIN_NORMAL_OPER;    // being normal operation
-uint8_t FLAG_HALT_NORMAL_OPER ;    // halt normal operation
-uint8_t FLAG_SEND_HEALTHCHECK ;    // send diagnostic health information
-uint8_t FLAG_SEND_NODECHECK   ;    // send node if and boot timestamp
-uint8_t FLAG_PRINT_TIMESTAMP  ;    // print timestamp in human readable format
+#include <Arduino.h>
 
-//
-#endif // end can_flags.h
+/** * Use extern to prevent multiple definition errors.
+ * These are now declarations, not definitions.
+ */
+extern uint8_t FLAG_SEND_INTRODUCTION;
+extern uint8_t FLAG_BEGIN_NORMAL_OPER;
+extern uint8_t FLAG_HALT_NORMAL_OPER;
+extern uint8_t FLAG_SEND_HEALTHCHECK;
+extern uint8_t FLAG_SEND_NODECHECK;
+extern uint8_t FLAG_PRINT_TIMESTAMP;
+
+#endif
