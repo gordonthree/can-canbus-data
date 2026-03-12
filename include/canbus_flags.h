56d54d4cd1c02908a8106008f78ce57c405b55cc
Gordon McLellan <gordonthree@gmail.com>
2026-03-12 06:33:18 -0400

    remove unused flags

--- include/canbus_flags.h
+++ include/canbus_flags.h
@@ -1,16 +1,3 @@
-#ifndef CAN_FLAGS_H
-#define CAN_FLAGS_H
+#pragma once
 
-#include <Arduino.h>
-
-/** * Use extern to prevent multiple definition errors.
- * These are now declarations, not definitions.
- */
-extern uint8_t FLAG_SEND_INTRODUCTION;
-extern uint8_t FLAG_BEGIN_NORMAL_OPER;
-extern uint8_t FLAG_HALT_NORMAL_OPER;
-extern uint8_t FLAG_SEND_HEALTHCHECK;
-extern uint8_t FLAG_SEND_NODECHECK;
-extern uint8_t FLAG_PRINT_TIMESTAMP;
-
-#endif
+/* Nothing here yet */
